f0a048a8b08d141e012f34e98d2577c20af04c6d
joaobroggine <joaovitorbrogginelopes@gmail.com>
2025-02-01 14:08:58 -0300

    Colocando sons nas teclas do teclado

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,9 +1,52 @@
-document.querySelectorAll(".drum").forEach(drum => {
-    drum.addEventListener("click", handleSound)
-})
+// Detectnado algo sendo pressionado
+
+document.querySelectorAll(".drum").forEach((drum) => {
+  drum.addEventListener("click", handleSound);
+});
 
 function handleSound() {
-    var audio = new Audio("sounds/tom-1.mp3")
-    audio.play()
+  var buttonInnerHTML = this.innerHTML;
+  makeSound(buttonInnerHTML);
+}
 
+//Detectando tecla especifica sendo pressionada
+
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    default:
+      console.log(buttonInnerHTML);
+      break;
+  }
 }
